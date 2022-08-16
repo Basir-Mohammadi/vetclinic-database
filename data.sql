608c778d0efbb03d8ad0bca877141014d0e29209
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-08-16 11:38:06 +0100

    Create data table

--- data.sql
+++ data.sql
@@ -1,5 +1,10 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (Id, Name, Date_of_birth, Escape_attempts, Neutered, Weight_kg) 
+VALUES (1, 'Agumon', '2020-02-03', 0, 'true', '10.23');
+INSERT INTO animals (Id, Name, Date_of_birth, Escape_attempts, Neutered, Weight_kg) 
+VALUES (2, 'Gabumon', '2018-15-11', 2, 'true', '8');
+INSERT INTO animals (Id, Name, Date_of_birth, Escape_attempts, Neutered, Weight_kg) 
+VALUES (3, 'Pikachu', '2021-07-01', 1, 'false', '15.04');
+INSERT INTO animals (Id, Name, Date_of_birth, Escape_attempts, Neutered, Weight_kg) 
+VALUES (4, 'Devimon', '2017-12-06', 5, 'true', '11');
