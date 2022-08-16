e560ce8efe2f98680104e6c88f838734ca4d9b11
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-08-16 11:38:36 +0100

    Create database

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    Id integer,
+    Name varchar(100),
+    Date_of_birth date,
+    Escape_attempts integer,
+    Neutered boolean,
+    Weight_kg decimal
 );
