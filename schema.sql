e66ece112fa0c837769a534ef76af7e7dd65bbd9
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-08-17 08:54:37 +0100

    Add new column to the table

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    Id integer,
+    Id int NOT NULL AUTO_INCREMENT,
     Name varchar(100),
     Date_of_birth date,
     Escape_attempts integer,
     Neutered boolean,
-    Weight_kg decimal
+    Weight_kg decimal,
+    Species varchar(100)
 );
